ce1e0a5590b881e14cc74d74edfa8dd12649a040
fworg64 <fworg64@gmail.com>
2017-08-07 21:01:07 -0500

    transformed model to distance and NAD setpoints, next step is distance and turn radius

--- locomotionv2.m
+++ locomotionv2.m
@@ -3,13 +3,13 @@
 
 %simulation variables
 dt = .01;
-time = 0:dt:1;
+time = 0:dt:5;
 
 Ul =1;
 Ur =1;
 
-SpeedInput = -4;
-SteeringInput = 1.5;
+SpeedInput = 0;
+SteeringInput = 0;
 
 robotState = [1;1;0];
 
@@ -27,6 +27,8 @@ sscPlotindex=1;
 
 posPlot = zeros(6, length(time));
 posPlotindex=1;
+
+linearRobotStateEstimate = [0;0;0];
 % 
 % linearRobotStateEstimate = [robotState;0];
 % linearRobotModelA = [0,0,0,-.1;
@@ -36,35 +38,33 @@ posPlotindex=1;
 % linearRobotB = [2,-1;0,-.5;0,-.10;0,0]; %linear model from velocitiy inputs, includes integral of 
 
 % 
-linearRobotStateEstimate = [robotState;0;0];
-linearRobotModelA = [0,0,0,1,0;%dx pos
-                    0,0,-.1,0,0;%dy pos
-                    0,0,0,0,-2;%dtheta
-                    0,0,0,0,0; %integral of Speed
-                    0,0,0,0,0];%integral of Steering
-linearRobotB = [1,0;
-                0,-2;
-                0,-3;
-                1,0;
-                0,1]; %Speed and steering input
+%linearRobotStateEstimate = [robotState;0;0];
+%linearRobotModelA = [0,0,0,1,0;%dx pos
+%                    0,0,-.1,0,0;%dy pos
+%                    0,0,0,0,-2;%dtheta
+%                    0,0,0,0,0; %integral of Speed
+%                    0,0,0,0,0];%integral of Steering
+%linearRobotB = [1,0;
+%                0,-2;
+%                0,-3;
+%                1,0;
+%                0,1]; %Speed and steering input
 
- linearRobotWithServoStates = [robotState;0;0;0;0];
- linearRobotWithServoA = [linearRobotModelA, zeros(5, 2);
-                         1,0,0,0,0,0,0;
-                         0,1,0,0,0,0,0];
- linearRobotWithServoB = [linearRobotB;zeros(2,2)];
+ linearRobotWithServoStates = [0;0;0;0];
+ linearRobotWithServoA = [0,0,0,0;
+                          0,0,0,0;
+                         1,0,0,0;
+                         0,1,0,0];
+ linearRobotWithServoB = [1,0;0,1;0,0;0,0];
  
- linearRobotWithServoSetpoint = -[0;0;0;0;0;3;1];
+linearRobotWithServoSetpoint = -[0;0;3;.2];
  
  %naturalroots = eig(linearRobotWithServoA)
  
- linearRobotQ = [100,0,0,0,0,0,0;
-                0,100,0,0,0,0,0;
-                0,0,1,0,0,0,0;
-                0,0,0,1,0,0,0;
-                0,0,0,0,1,0,0;
-                0,0,0,0,0,100,0;
-                0,0,0,0,0,0,100];
+ linearRobotQ = [100,0,0,0;
+                0,100,0,0,;
+                0,0,10,0;
+                0,0,0,10];
   linearRobotR = [1,0;0,1];
   
   Kx = lqr(linearRobotWithServoA, linearRobotWithServoB, linearRobotQ, linearRobotR)
@@ -78,15 +78,15 @@ for t = time;
   drobotState = robotdynamics(Ul, Ur, robotState(3), dt);
   robotState = robotState + drobotState;
     
-   dlinearRobot = linearRobotModelA * linearRobotStateEstimate + linearRobotB*[SpeedInput;SteeringInput];
-   linearRobotStateEstimate = linearRobotStateEstimate + dlinearRobot*dt;
+   %dlinearRobot = linearRobotModelA * linearRobotStateEstimate + linearRobotB*[SpeedInput;SteeringInput];
+   %linearRobotStateEstimate = linearRobotStateEstimate + dlinearRobot*dt;
    
    dlinearRobotWithServo = linearRobotWithServoA * linearRobotWithServoStates + linearRobotWithServoB * [SpeedInput;SteeringInput] + linearRobotWithServoSetpoint;
    linearRobotWithServoStates = linearRobotWithServoStates + dlinearRobotWithServo*dt;
    
-   input = -Kx(:,1:5)*linearRobotWithServoStates(1:5) - Kx(:,6:7)*linearRobotWithServoStates(6:7);
-  % SpeedInput = input(1);
-  % SteeringInput = input(2);
+   input = -Kx(:,1:2)*linearRobotWithServoStates(1:2) - Kx(:,3:4)*linearRobotWithServoStates(3:4);
+  SpeedInput = input(1);
+  SteeringInput = input(2);
     
   %plot robot
   subplot(2,2,1);
@@ -119,11 +119,11 @@ for t = time;
   sscPlotindex = sscPlotindex +1;
   
   %plot graph of x y and theta
-  subplot(2,2,4);
-  posPlot(:,posPlotindex) = [robotState(1:3)', linearRobotStateEstimate(1:3)'];
-  plot([0:dt:t], posPlot(1:3, 1:posPlotindex), '-', [0:dt:t], posPlot(4:6, 1:posPlotindex), '--');
-  legend('X', 'Y', '\theta','X estimate','Y estimate', '\theta estimate');
-  posPlotindex = posPlotindex+1; 
+  %subplot(2,2,4);
+  %posPlot(:,posPlotindex) = [robotState(1:3)', linearRobotStateEstimate(1:3)'];
+  %plot([0:dt:t], posPlot(1:3, 1:posPlotindex), '-', [0:dt:t], posPlot(4:6, 1:posPlotindex), '--');
+  %legend('X', 'Y', '\theta','X estimate','Y estimate', '\theta estimate');
+  %posPlotindex = posPlotindex+1; 
   
   
 end
